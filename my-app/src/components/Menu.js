119c642394d200cfdbbd66dd3325c6b0c39ab3c8
Công Bằng <92241569+congbangitiu@users.noreply.github.com>
2023-02-26 21:21:29 +0700

    update button Restart

--- my-app/src/components/Menu.js
+++ my-app/src/components/Menu.js
@@ -329,10 +329,12 @@ function Menu() {
         <div className="Menu">
           <div className="scoreNoti">Your score is: {point}</div>
           <button className="NextFinishRes restart">
-            <a href="http://localhost:3000/">Restart</a>
-            <div class="icon">
-              <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
-            </div>
+            <a href="http://localhost:3000/">
+              Restart
+              <div class="icon">
+                <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+              </div>
+            </a>
           </button>
         </div>
       )}
